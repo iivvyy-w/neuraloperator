3306e752844a5d0e8b7d51a35db1531fed450b69
Yaxuan Wang <yw6221@ic.ac.uk>
2025-01-31 11:35:59 +0000

    backward test

--- neuralop/models/tests/test_bc.py
+++ neuralop/models/tests/test_bc.py
@@ -25,20 +25,16 @@ def test_backward(m):
     torch.manual_seed(m*3434)
     batch_size = 5
 
-    A = torch.randn(m, m, requires_grad = True)
-    y = torch.randn(batch_size, m, requires_grad = True)
-    b = torch.randn(batch_size, m)
-    cl = ConstraintLayer(A, b)
-    y_star = cl.forward(y)
-
-    # Generate random gradient output
-    grad_output = torch.randn_like(y_star)
-
-    # Compute backward gradients using PyTorch autograd
-    grad_y_manual, _, _ = ConstraintFunction.backward(None, grad_output)
-
-    # Compute numerical gradients with gradcheck
+    A = torch.randn(m, m, dtype=torch.double)
+    y = torch.randn(batch_size, m, dtype=torch.double, requires_grad=True)
+    b = torch.randn(batch_size, m, dtype=torch.double)
     assert torch.autograd.gradcheck(ConstraintFunction.apply, (y, A, b), eps=1e-6, atol=1e-4)
+
+    y_star = ConstraintFunction.apply(y, A, b)
+    grad_output = torch.randn_like(y_star)
+    grad_y = torch.autograd.grad(y_star, y, grad_outputs=grad_output)[0]
+    forward_grad_output = ConstraintFunction.apply(grad_output, A, torch.zeros_like(b))
+    assert torch.allclose(grad_y, forward_grad_output, atol=1e-4), "Backward is not equivalent to Forward"
 
 if __name__ == '__main__':
     import sys
